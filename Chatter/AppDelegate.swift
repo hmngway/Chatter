0239f193eeee1ef04f3b5d42d1651ed8ae74cb26
hmngway <kentjacobm@gmail.com>
2017-10-12 21:51:27 -0400

    Initial Commit

---
+++ Chatter/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Chatter
+//
+//  Created by Harley on 10/12/17.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
